b0b1034d0e57543f331465f6eacafc93b6e1da9f
ArriagaReyes <hello@arriagareyes.me>
2024-08-10 19:27:50 -0400

    Moved server into its own repo.

--- src/index.js
+++ src/index.js
@@ -12,4 +12,32 @@ window.onload = (event) => {
     }
 
     const animations = new Animations();
+
+    const quantity = document.getElementById('quantity');
+
+
+    const checkout = document.getElementById('checkout-button');
+    checkout.addEventListener('click', (e) => {
+        e.preventDefault();
+        console.log('Checking out');
+
+        fetch('http://192.168.42.11:1234/create-checkout-session', {
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                items: [
+                    { id: 1, quantity: Number(quantity.value) }
+                ]
+            }),
+        }).then(res => {
+            if(res.ok) return res.json();
+            return res.json().then(json => Promise.reject(json));
+        }).then(({ url }) => {
+            window.location = url;
+        }).catch(e => {
+            console.error(e.error);
+        });
+    });
 }
