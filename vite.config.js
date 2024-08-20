c4f85cfce15f90e52d4cac60b4b6677621b3dbb2
ArriagaReyes <hello@arriagareyes.me>
2024-08-20 14:10:22 -0400

    Configured urls.

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,13 @@ export default {
     {
         outDir: '../dist', // Output in the dist/ folder
         emptyOutDir: true, // Empty the folder first
-        sourcemap: true // Add sourcemap
+        sourcemap: true, // Add sourcemap
+        rollupOptions: {
+            input: {
+                home: 'src/index.html',
+                success: 'src/success.html',
+            }
+        }
     },
     plugins:
     [
